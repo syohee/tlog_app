d0c55c9b4e82b4a2c54e5de354e5381c4024490f
seonghee <tjdgmll2@naver.com>
2019-01-23 03:26:54 +0000

    init6

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,6 @@ module TlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.cache_classes = true
-    config.serve_static_assets = true
-    config.assets.compile = true
-    config.assets.digest = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
